trinodb/trino

Add anonymized query plan in json format to QueryCompletedEvent

core/trino-main/src/main/java/io/trino/event/QueryMonitor.java
@@ -67,6 +67,8 @@ import io.trino.sql.planner.plan.PlanFragmentId;
 import io.trino.sql.planner.plan.PlanNode;
 import io.trino.sql.planner.plan.PlanNodeId;
 import io.trino.sql.planner.plan.PlanVisitor;
+import io.trino.sql.planner.planprinter.Anonymizer;
+import io.trino.sql.planner.planprinter.CounterBasedAnonymizer;
 import io.trino.sql.planner.planprinter.NoOpAnonymizer;
 import io.trino.sql.planner.planprinter.ValuePrinter;
 import io.trino.transaction.TransactionId;
@@ -87,6 +89,7 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static io.trino.execution.QueryState.QUEUED;
 import static io.trino.execution.StageInfo.getAllStages;
+import static io.trino.sql.planner.planprinter.PlanPrinter.jsonDistributedPlan;
 import static io.trino.sql.planner.planprinter.PlanPrinter.textDistributedPlan;
 import static java.lang.Math.max;
 import static java.lang.Math.toIntExact;
@@ -160,12 +163,13 @@ public class QueryMonitor
                                 ImmutableList.of(),
                                 queryInfo.getSelf(),
                                 Optional.empty(),
+                                Optional.empty(),
                                 Optional.empty())));
     }
 
     public void queryImmediateFailureEvent(BasicQueryInfo queryInfo, ExecutionFailureInfo failure)
     {
-        eventListenerManager.queryCompleted(new QueryCompletedEvent(
+        eventListenerManager.queryCompleted(requiresAnonymizedPlan -> new QueryCompletedEvent(
                 new QueryMetadata(
                         queryInfo.getQueryId().toString(),
                         queryInfo.getSession().getTransactionId().map(TransactionId::toString),
@@ -176,6 +180,7 @@ public class QueryMonitor
                         ImmutableList.of(),
                         ImmutableList.of(),
                         queryInfo.getSelf(),
+                        Optional.empty(),
                         Optional.empty(),
                         Optional.empty()),
                 new QueryStatistics(
@@ -234,9 +239,9 @@ public class QueryMonitor
     public void queryCompletedEvent(QueryInfo queryInfo)
     {
         QueryStats queryStats = queryInfo.getQueryStats();
-        eventListenerManager.queryCompleted(
+        eventListenerManager.queryCompleted(requiresAnonymizedPlan ->
                 new QueryCompletedEvent(
-                        createQueryMetadata(queryInfo),
+                        createQueryMetadata(queryInfo, requiresAnonymizedPlan),
                         createQueryStatistics(queryInfo),
                         createQueryContext(
                                 queryInfo.getSession(),
@@ -253,8 +258,9 @@ public class QueryMonitor
         logQueryTimeline(queryInfo);
     }
 
-    private QueryMetadata createQueryMetadata(QueryInfo queryInfo)
+    private QueryMetadata createQueryMetadata(QueryInfo queryInfo, boolean requiresAnonymizedPlan)
     {
+        Anonymizer anonymizer = requiresAnonymizedPlan ? new CounterBasedAnonymizer() : new NoOpAnonymizer();
         return new QueryMetadata(
                 queryInfo.getQueryId().toString(),
                 queryInfo.getSession().getTransactionId().map(TransactionId::toString),
@@ -265,7 +271,8 @@ public class QueryMonitor
                 queryInfo.getReferencedTables(),
                 queryInfo.getRoutines(),
                 queryInfo.getSelf(),
-                createTextQueryPlan(queryInfo),
+                createTextQueryPlan(queryInfo, anonymizer),
+                createJsonQueryPlan(queryInfo, anonymizer),
                 queryInfo.getOutputStage().flatMap(stage -> stageInfoCodec.toJsonWithLengthLimit(stage, maxJsonLimit)));
     }
 
@@ -346,7 +353,7 @@ public class QueryMonitor
                 retryPolicy.toString());
     }
 
-    private Optional<String> createTextQueryPlan(QueryInfo queryInfo)
+    private Optional<String> createTextQueryPlan(QueryInfo queryInfo, Anonymizer anonymizer)
     {
         try {
             if (queryInfo.getOutputStage().isPresent()) {
@@ -355,13 +362,33 @@ public class QueryMonitor
                         queryInfo.getQueryStats(),
                         new ValuePrinter(metadata, functionManager, queryInfo.getSession().toSession(sessionPropertyManager)),
                         false,
-                        new NoOpAnonymizer()));
+                        anonymizer));
             }
         }
         catch (Exception e) {
             // Sometimes it is expected to fail. For example if generated plan is too long.
             // Don't fail to create event if the plan cannot be created.
             log.warn(e, "Error creating explain plan for query %s", queryInfo.getQueryId());
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> createJsonQueryPlan(QueryInfo queryInfo, Anonymizer anonymizer)
+    {
+        try {
+            if (queryInfo.getOutputStage().isPresent()) {
+                return Optional.of(jsonDistributedPlan(
+                        queryInfo.getOutputStage().get(),
+                        queryInfo.getSession().toSession(sessionPropertyManager),
+                        metadata,
+                        functionManager,
+                        anonymizer));
+            }
+        }
+        catch (Exception e) {
+            // Sometimes it is expected to fail. For example if generated plan is too long.
+            // Don't fail to create event if the plan cannot be created.
+            log.warn(e, "Error creating anonymized json plan for query %s", queryInfo.getQueryId());
         }
         return Optional.empty();
     }

treasure-data/presto

presto-main/src/main/java/io/prestosql/event/QueryMonitor.java
@@ -78,6 +78,7 @@ import java.util.stream.Collectors;
 
 import static io.prestosql.execution.QueryState.QUEUED;
 import static io.prestosql.execution.StageInfo.getAllStages;
+import static io.prestosql.sql.planner.planprinter.PlanPrinter.jsonDistributedPlan;
 import static io.prestosql.sql.planner.planprinter.PlanPrinter.textDistributedPlan;
 import static java.lang.Math.max;
 import static java.lang.Math.toIntExact;
@@ -144,6 +145,7 @@ public class QueryMonitor
                                 ImmutableList.of(),
                                 queryInfo.getSelf(),
                                 Optional.empty(),
+                                Optional.empty(),
                                 Optional.empty())));
     }
 
@@ -160,6 +162,7 @@ public class QueryMonitor
                         ImmutableList.of(),
                         ImmutableList.of(),
                         queryInfo.getSelf(),
+                        Optional.empty(),
                         Optional.empty(),
                         Optional.empty()),
                 new QueryStatistics(
@@ -234,6 +237,7 @@ public class QueryMonitor
                 queryInfo.getRoutines(),
                 queryInfo.getSelf(),
                 createTextQueryPlan(queryInfo),
+                createJsonQueryPlan(queryInfo),
                 queryInfo.getOutputStage().flatMap(stage -> stageInfoCodec.toJsonWithLengthLimit(stage, maxJsonLimit)));
     }
 
@@ -318,6 +322,24 @@ public class QueryMonitor
             // Sometimes it is expected to fail. For example if generated plan is too long.
             // Don't fail to create event if the plan cannot be created.
             log.warn(e, "Error creating explain plan for query %s", queryInfo.getQueryId());
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> createJsonQueryPlan(QueryInfo queryInfo)
+    {
+        try {
+            if (queryInfo.getOutputStage().isPresent()) {
+                return Optional.of(jsonDistributedPlan(
+                        queryInfo.getOutputStage().get(),
+                        queryInfo.getSession().toSession(sessionPropertyManager),
+                        metadata));
+            }
+        }
+        catch (Exception e) {
+            // Sometimes it is expected to fail. For example if generated plan is too long.
+            // Don't fail to create event if the plan cannot be created.
+            log.warn(e, "Error creating anonymized json plan for query %s", queryInfo.getQueryId());
         }
         return Optional.empty();
     }